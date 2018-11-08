cbcd7af3ff590071fc45c577e6bc6e84b5690d61
Tobias Frantsen <tof@amsiq.com>
2018-11-08 11:42:29 +0100

    create lib project via pod

---
+++ Example/Pods/Target Support Files/Pods-iOSMediachat_Example/Pods-iOSMediachat_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOSMediachat_Example : NSObject
+@end
+@implementation PodsDummy_Pods_iOSMediachat_Example
+@end
---
+++ Example/Pods/Target Support Files/iOSMediachat/iOSMediachat-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_iOSMediachat : NSObject
+@end
+@implementation PodsDummy_iOSMediachat
+@end
